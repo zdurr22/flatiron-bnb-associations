6ee3b9c5066dbed34dd5e023eb54fceab2cd89e7
zdurr22 <zdurr22@gmail.com>
2021-07-28 16:16:35 -0400

    commit tree 9507f0f398cc567abc646326e811de1da8f87fc3

--- db/migrate/20210728191958_create_listings.rb
+++ db/migrate/20210728191958_create_listings.rb
@@ -5,7 +5,7 @@ class CreateListings < ActiveRecord::Migration[5.0]
       t.string :listing_type
       t.string :title
       t.text :description
-      t.decimal :price, :precision => 8, :scale => 2
+      t.integer :price
       t.integer :neighborhood_id
       t.integer :host_id
       t.timestamps
